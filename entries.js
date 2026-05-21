313fd3ee67ae0a68a9ae6d1c07411e0d50e2bcac
Ryan <rfisk27@gmail.com>
2026-05-20 21:29:57 -0400

    Update blog entries and trips

--- entries.js
+++ entries.js
@@ -1,12 +1,5 @@
 const entries = [
   {
-    "date": "2026-05-17",
-    "location": "Testing in Paris",
-    "latitude": 0,
-    "longitude": 0,
-    "notes": "test"
-  },
-  {
     "date": "2026-05-16",
     "location": "Example City, Country",
     "latitude": 0,
